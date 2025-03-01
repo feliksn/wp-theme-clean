1f4389b59c772892f6de0593d9bdba4fd503b54d
Feliks N <feliks.nvk@gmail.com>
2025-03-01 14:19:29 +0100

    Исправить пути к файлам библиотеки

--- functions.php
+++ functions.php
@@ -4,12 +4,12 @@
 add_action('wp_enqueue_scripts', 'add_scripts');
 function add_scripts() {
 	// Theme sytles
-	wp_enqueue_style( 'bs', get_template_directory_uri().'/css/bootstrap.min.css' );
-	wp_enqueue_style( 'bs-icons', get_template_directory_uri().'/css/bootstrap-icons.min.css' );
-	wp_enqueue_style( 'main', get_template_directory_uri().'/css/main.css');
+	wp_enqueue_style( 'bs', get_template_directory_uri() .'/lib/bootstrap/bootstrap.min.css' );
+	wp_enqueue_style( 'bs-icons', get_template_directory_uri() . '/lib/bootstrap/bootstrap-icons.min.css' );
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/css/main.css');
 	// Theme scripts
-	wp_enqueue_script('jq', get_template_directory_uri(). '/js/jquery-3.7.1.min.js',[],'',true);
-	wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.bundle.min.js',[],'',true);
-	wp_enqueue_script('main', get_template_directory_uri().'/js/main.js');
+	wp_enqueue_script('jq', get_template_directory_uri() . '/lib/jquery/jquery-3.7.1.min.js',[],'',true);
+	wp_enqueue_script('bootstrap', get_template_directory_uri().'/lib/bootstrap/bootstrap.bundle.min.js',[],'',true);
+	wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js');
 }
 ?>
