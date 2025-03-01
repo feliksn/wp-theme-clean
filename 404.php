b5043033377a6e40c6cdfb98ab55cf7d1d5dc3ee
Feliks N <feliks.nvk@gmail.com>
2025-03-01 14:17:12 +0100

    Clean a 404 page

--- 404.php
+++ 404.php
@@ -1,19 +1,6 @@
-<?php
-/**
- * Error page (404.php)
- * @package WordPress
- * @subpackage wp-theme-clean
- */
-get_header(); ?>
-<section>
-	<div class="container">
-		<div class="row">
-			<div>
-				<h1>404!</h1>
-				<p>The page doesn't exists</p>
-			</div>
-			
-		</div>
-	</div>
-</section>
-<?php get_footer(); ?>
+<?php get_header(); ?>
+
+<h1>404!</h1>
+<p>Страница не была найдена!</p>
+
+<?php get_footer(); ?>	
