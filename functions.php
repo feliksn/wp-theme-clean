c9bfdee72a8354ba88d9905a9f9c6c286514b03d
Feliks N <feliks.nvk@gmail.com>
2025-03-03 14:30:27 +0100

    Добавить стили к  элементами и ссылкам нав.меню

--- functions.php
+++ functions.php
@@ -3,6 +3,22 @@
 // Добавить поддержку меню
 add_theme_support("menus");
 
+// Изменить класс для <li> в меню навигации под класс bootstrap
+add_filter( 'nav_menu_css_class', 'change_menu_item_css_classes', 10, 4 );
+function change_menu_item_css_classes( $classes, $item, $args, $depth ) {
+	$classes = ['nav-item'];
+	return $classes;
+}
+
+// Изменить класс для <a> в меню навигации под класс bootstrap
+add_filter( 'nav_menu_link_attributes', 'filter_nav_menu_link_attributes', 10, 4 );
+function filter_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
+		$atts['class'] = 'nav-link';
+
+	return $atts;
+}
+
+// Добавить стили и скрипты
 add_action('wp_enqueue_scripts', 'add_scripts');
 function add_scripts() {
 	// Theme sytles
